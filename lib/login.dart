58756d04e9f2582fe87a207d9c00a4b54f29bbf1
Vaiiery <55927707+Vaiiery@users.noreply.github.com>
2021-04-02 14:47:47 -0500

    primer setup

---
+++ lib/login.dart
@@ -0,0 +1,140 @@
+
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'email_service.dart';
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      MaterialApp(
+      title: 'Dr. Soler',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ), //ThemeData
+      home: LoginPage(),
+    ); //MaterialApp
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  final emailService= EmailService();
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var _emailForm = GlobalKey<FormState>();
+  var _codeForm = GlobalKey<FormState>();
+  //intancia de controller par ael mail tomar el valor del correo
+  var _emailController = TextEditingController();
+  var authCode = 0;//almacena el valor del codigo
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dr. Soler"),
+      ),
+      body: authCode > 0 ? codeForm() : emailForm(),
+    );
+  }
+
+  Form emailForm() {
+    return Form(
+      key: _emailForm,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: 300,
+                child: TextFormField(
+                  controller: _emailController,
+                  validator: (value) => value.isEmpty
+                      ? "Correo Requerido"
+                      : EmailValidator.validate(value)
+                          ? null
+                          : "Correo Incorrecto",
+                  decoration: InputDecoration(labelText: "Email"),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () async {
+                    if (_emailForm.currentState.validate()) {
+                      var code=await widget.emailService.sendSignInCode(_emailController.text);
+                  
+                      setState(() {
+                        authCode = code;
+                        print(code);
+                      });
+                    }
+                  },
+                  child: Text("Entrar")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Form codeForm() {//cambio de pantalla para validar el codigo
+    return Form(
+      key: _codeForm,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Revisa tu correo",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+                width: 100,
+                child: TextFormField(
+                  validator: (value) =>
+                      authCode > 0 && int.tryParse(value) == authCode
+                          ? null
+                          : "incorrecto",
+                  keyboardType: TextInputType.number,
+                  maxLength: 4,
+                  decoration: InputDecoration(
+                    labelText: "codigo",
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () async {
+                    if (_codeForm.currentState.validate()) {
+                      var superPassword = "Dr.Soler7788";
+
+                      try {
+                        await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: superPassword);
+                      } catch (e) {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: superPassword);
+
+                        print(e);
+                      }
+                    }
+                  },
+                  child: Text("Entrar")),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 //import 'dart:math';
-
-import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+//Authentificación de la Base de Datos.
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 //le puse en nombre al el paquete para luego llmarlos mas adelante
 
-import 'email_service.dart';
+
+import 'login.dart';
 
 
 Future main() async {
@@ -16,122 +17,44 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.iniializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == connectionState.done) {
+            return StreamBuilder<User>(
+              stream:FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+               if (snapshot.data == null) {
+                 return LoginApp();
+               }
+               return HomeApp();
+             });
+          }// StreamBuilder
+
+          return CircularProgressIndicator();
+        })
+  }
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dr. Soler',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  final emailService= EmailService();
-  
-  LoginPage({Key key}) : super(key: key);
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  var _emailForm = GlobalKey<FormState>();
-  var _codeForm = GlobalKey<FormState>();
-  //intancia de controller par ael mail tomar el valor del correo
-  var _emailController = TextEditingController();
-  var authCode = 0;//almacena el valor del codigo
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dr. Soler"),
-      ),
-      body: authCode > 0 ? codeForm() : emailForm(),
-    );
-  }
-
-  Form emailForm() {
-    return Form(
-      key: _emailForm,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-                width: 300,
-                child: TextFormField(
-                  controller: _emailController,
-                  validator: (value) => value.isEmpty
-                      ? "Correo Requerido"
-                      : EmailValidator.validate(value)
-                          ? null
-                          : "Correo Incorrecto",
-                  decoration: InputDecoration(labelText: "Email"),
-                )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                  onPressed: () async {
-                    if (_emailForm.currentState.validate()) {
-                      var code=await widget.emailService.sendSignInCode(_emailController.text);
-                  
-                      setState(() {
-                        authCode = code;
-                        print(code);
-                      });
-                    }
-                  },
-                  child: Text("Entrar")),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Form codeForm() {//cambio de pantalla para validar el codigo
-    return Form(
-      key: _codeForm,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Revisa tu correo",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-                width: 100,
-                child: TextFormField(
-                  validator: (value) =>
-                      authCode > 0 && int.tryParse(value) == authCode
-                          ? null
-                          : "incorrecto",
-                  keyboardType: TextInputType.number,
-                  maxLength: 4,
-                  decoration: InputDecoration(
-                    labelText: "codigo",
-                  ),
-                )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextButton(
-                  onPressed: () {
-                    if (_codeForm.currentState.validate()) {
-                      print("entraste");
-                    }
-                  },
-                  child: Text("Entrar")),
-            )
-          ],
-        ),
-      ),
-    );
+      ), //ThemeData
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Citas"),
+        ), //AppBar
+      ), //Scaffold
+    ); //MaterialApp
   }
 }

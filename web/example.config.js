eb140b9223c9cd7bda9f94fcceaddfc908747088
Vaiiery <55927707+Vaiiery@users.noreply.github.com>
2021-04-01 20:47:16 -0500

    Configuracion de la base de datos

---
+++ web/example.config.js
@@ -0,0 +1,13 @@
+
+//TODO: Actualiza con tus variables de configuracion y remueve el example."nombre del archivo" 
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };

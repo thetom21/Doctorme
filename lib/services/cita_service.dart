6c9d8da3e0b07114c51b442927b196bc7b8552aa
Vaiiery <55927707+Vaiiery@users.noreply.github.com>
2021-04-14 21:48:38 -0400

    Arreglo dde los turnos que estaban fallando

--- lib/services/cita_service.dart
+++ lib/services/cita_service.dart
@@ -4,22 +4,26 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/cita.dart';
-import '../models/cita.dart';
-import '../models/cita.dart';
 
 class CitaService{
 
   Future<void> create(String email, DateTime day) async {
     try{
+      //Se pone le pone el turno que le toca al paciente.
       var citasOfTheDay = await getByDay(day);
       var turn = 1;
-      citasOfTheDay.forEach((element) => turn = max(element.turn+1,turn));
-      await FirebaseFirestore.instance.collection('citas').add({
-        'email': email, 'day': day, 'turn': 1, 'status': 'pendiente'});
+      citasOfTheDay.forEach((element) => turn = max(element.turn + 1, turn));
+
+      await FirebaseFirestore.instance.collection('citas').add({ //Instaciar base de datos en el campo de lista.
+        'email': email, 'day': day, 'turn': turn, 'status': 'pendiente'});
+
+        /*f( turn > 3) {
+          print('Este dia esta lleno, seleccione otro día');
+        } */
     } catch(e) {
 
     }
-  }
+  } 
 
   Future<List<Cita>> getByEmail(String email) async {
     try{
@@ -43,19 +47,19 @@ class CitaService{
     
   }
 
-  Future<void> cancel(Cita cita) async {
+   Future<void> cancel(Cita cita) async {
     try{
-      
       await FirebaseFirestore.instance
           .doc(cita.reference.path)
-          .update({'status': 'cancelado', 'turn':0});
-      await updateCitasAfterTurn(cita.day, cita.turn);
+          .update({'status': 'cancelado', 'turn':  0});
+
+      await updateCitasAfterTurn(cita.day, cita.turn); //Actualizar la cita despues de un turno.
           } catch (e) {
             print(e);
           }
         }
       
-        Future<List<Cita>> getByDay(DateTime day) async {
+        Future<List<Cita>> getByDay(DateTime day) async{
           try{
             var nextDay = day.add(Duration(days: 1));
             var snapshot = await FirebaseFirestore.instance
@@ -75,34 +79,35 @@ class CitaService{
             print(e);
             return null;
           }
-      
-      
         }
-        //Metodo update de los turnos de la citas
-        Future<void>updateCitasAfterTurn(DateTime day, int turn) async {
-          try{
-            var dbCitas = await getByDay(day);
-           
-            List<Cita> citas = [];
-            dbCitas.forEach((c) {
-              if(c.turn>= turn){
-                citas.add(c);
-              }
-            });
-            //Organiza por turno
-            citas.sort((a, b) => a.turn.compareTo(b.turn));
-      
-            List<Future>futures = [];
-            citas.forEach((element) {
-              futures.add(FirebaseFirestore.instance.doc(element.reference.path).update({'turn': turn}));
-              turn++;// agregandole valor a la actualizacion de turnos
-             });
-             await Future.wait(futures);
-          } catch (e) {
-            print(e);
-            return null;
+
+    Future<void> updateCitasAfterTurn(DateTime day, int turn) async {
+      try{
+        var dbCitas = await getByDay(day); //Se busca todas las citas que son de ese dia.
+        List<Cita> citas = [];
+        //Filtra para tener todas las que van desde ese turno que se acaba de cancelar.
+        dbCitas.forEach((c) {
+          if (c.turn >= turn) {
+            citas.add(c);
           }
-      
-      
-        }
+        });
+           //Organizar por turno.
+        citas.sort((a, b) => b.turn.compareTo(a.turn));
+
+        List<Future> futures = [];
+        //Se le agrega el valor que va hacer en la actualizacion.
+        citas.forEach((element) {
+          futures.add(
+            FirebaseFirestore.instance
+            .doc(element.reference.path)
+            .update({'turn': turn}));
+            turn++; // Aqui le va asignando un numero a partir de la que se cancela.
+          });
+        
+        await Future.wait(futures); //Va a la base de dados y actualiza todas las citas.
+      } catch (e) {
+        print(e);
+        return null;
+      }
+   }
 }
